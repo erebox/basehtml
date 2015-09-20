838202b07534a0393777b1db9a2bf51baa42f02e
erebox <erebox@yahoo.it>
2015-09-20 11:04:18 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<? $titolo="api.erebox.it"; $year=date("Y"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<head>
+	<title><?=$titolo?></title>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<link rel="stylesheet" href="base.css" type="text/css" media="all" />
+</head>
+
+<body>
+	<div class="cont">
+		<div class="head"><?=$titolo?></div>
+		<div class="page"><img alt="Logo" src="erebox_logo.png"></div>
+	</div>
+	<div class="foot">&copy; <?=$year;?> Erebox</div>
+</body>
+
+</html>
