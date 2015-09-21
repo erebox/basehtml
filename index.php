fffe69c4a4f5993dbd5de0aaeab502fbf1a20323
Nicola Antonazzo <nicola.antonazzo@corvallis.it>
2015-09-21 08:48:17 +0200

    Aggiunto Logo e create subdir

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
-<? $titolo="erebox.it"; $year=date("Y"); ?>
+<? 
+$titolo="erebox.it"; 
+$year=date("Y");
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 
 <head>
 	<title><?=$titolo?></title>
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
-	<link rel="stylesheet" href="base.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/base.css" type="text/css" media="all" />
 </head>
 
 <body>
 	<div class="cont">
 		<div class="head"><?=$titolo?></div>
-		<div class="page"><img alt="Logo" src="erebox_logo.png"></div>
+		<div class="page"><img alt="Logo" src="img/logo.png"></div>
 	</div>
 	<div class="foot">&copy; <?=$year;?> Erebox</div>
 </body>
